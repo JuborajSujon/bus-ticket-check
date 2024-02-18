fb2562b8954c52a3221172694f50f1dd1d609854
Juboraj Sujon <mdsujon72@gmail.com>
2024-02-19 01:08:18 +0600

    Added cupon general function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
+// Cuopon Apply Button
 
+function couponAppyBtn() {
+  const cuoponContainer = document.getElementById("coupon-container");
+  const cuoponSingle = getInnerText("coupon-single");
+  const cuoponDuoble = getInnerText("cuopon-duoble");
+
+  inputCuoponValue = cuoponContainer.firstElementChild.value;
+
+  if (cuoponSingle === inputCuoponValue) {
+    cuoponContainer.classList.add("hidden");
+  } else if (cuoponDuoble === inputCuoponValue) {
+    cuoponContainer.classList.add("hidden");
+  } else if (inputCuoponValue === "") {
+    return;
+  } else {
+    alert("Invalid coupon code");
+    cuoponContainer.firstElementChild.value = "";
+  }
+}
+
+function selectSeatPosition() {
+  const seatContainer = document.getElementById("seat-container");
+}
---
+++ js/utility.js
@@ -0,0 +1,4 @@
+// Get Inner Text from an element
+function getInnerText(element) {
+  return document.getElementById(element).innerText;
+}
