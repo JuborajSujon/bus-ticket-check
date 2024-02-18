626badc242b5458d205241a6e46928cb20e4ac1a
Juboraj Sujon <mdsujon72@gmail.com>
2024-02-19 04:01:41 +0600

    Added max qty select function

--- js/app.js
+++ js/app.js
@@ -20,15 +20,59 @@ function couponAppyBtn() {
 }
 
 // Selected Seat Container
-const seatContainer = document.getElementById("seat-container");
+const seatContainer = document.getElementsByClassName("seat-item");
 
-// Add event listener to the container
-seatContainer.addEventListener("click", (e) => {
-  // Check if the clicked element is a seat-item
-  if (e.target.classList.contains("seat-item")) {
-    // change the property of the clicked element
-    e.target.classList.remove("bg-slate-300");
-    e.target.classList.add("bg-[#1DD100]");
-    e.target.classList.add("text-white");
-  }
-});
+// Selected Seat remainer
+const seatRemainer = document.getElementById("seat-remainer");
+let seatRemainerValue = parseInt(getInnerText("seat-remainer"));
+
+// Selected Seat to cart
+const seatToCart = document.getElementById("seat-to-cart");
+let seatToCartValue = parseInt(getInnerText("seat-to-cart"));
+
+// Get The Ticket Price
+const ticketPrice = parseInt(getInnerText("ticket-price"));
+
+// Select the seat ticket price
+const seatTicketContainer = document.getElementById("seat-ticket-container");
+
+for (const item of seatContainer) {
+  // Add event listener to the container
+  item.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    //Check if the seat is already selected
+    if (!e.target.classList.contains("clicked")) {
+      if (seatToCartValue < 4) {
+        seatRemainer.innerText = seatRemainerValue - 1;
+        seatToCart.innerText = seatToCartValue + 1;
+        seatRemainerValue--;
+        seatToCartValue++;
+        e.target.classList.add("clicked");
+
+        // change the property of the clicked element
+        e.target.classList.remove("bg-slate-200");
+        e.target.classList.add("bg-[#1DD100]");
+        e.target.classList.add("text-white");
+
+        // User click on a seat
+        const userPressedSeat = e.target.innerText;
+
+        // Add selected seat to cart
+        addItemToCart(seatTicketContainer, userPressedSeat, ticketPrice);
+      } else {
+        alert("No seat left");
+        const maxQtyAlert = document.getElementById("max-qty-alert");
+        maxQtyAlert.classList.remove("invisible");
+        maxQtyAlert.classList.add("visible");
+        const refreshInvervalId = setInterval(() => {
+          maxQtyAlert.classList.remove("visible");
+          maxQtyAlert.classList.add("invisible");
+          console.log("Hello");
+        }, 2000);
+        clearInterval(refreshInvervalId);
+        return;
+      }
+    }
+  });
+}
--- js/utility.js
+++ js/utility.js
@@ -2,3 +2,42 @@
 function getInnerText(element) {
   return document.getElementById(element).innerText;
 }
+
+// Add item to cart
+function addItemToCart(parentContainer, elementInnerText, ticketPrice) {
+  // Create the container div
+  const containerDiv = document.createElement("div");
+
+  // Add class to the container
+  containerDiv.classList.add(
+    "flex",
+    "justify-between",
+    "items-center",
+    "text-clr-primary",
+    "opacity-60"
+  );
+
+  // Create the span elements
+  const span1 = document.createElement("span");
+  span1.textContent = elementInnerText;
+
+  const span2 = document.createElement("span");
+  span2.textContent = "Economy";
+
+  const span3 = document.createElement("span");
+  span3.textContent = ticketPrice;
+
+  // Append the span elements to the container
+  containerDiv.appendChild(span1);
+  containerDiv.appendChild(span2);
+  containerDiv.appendChild(span3);
+
+  // Append the container to the cart
+  parentContainer.appendChild(containerDiv);
+}
+
+/* <div class="flex justify-between items-center *:text-clr-primary *:opacity-60">
+  // <span>C2</span>
+  // <span>Economy</span>
+  // <span>550</span>
+// </div>; */
